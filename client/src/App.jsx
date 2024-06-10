c3d9145e35d4f6589b873a505104cc0d5640b7ec
Priyanshu <20bec076@iiitdmj.ac.in>
2024-06-10 14:49:31 +0530

    pages done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { BrowserRouter , Route , Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About';
+import Dashboard from './pages/Dashboard';
+import SignIn from './pages/SignIn';
+import Signup from './pages/Signup';
+import Projects from './pages/Projects';
 
 const App = () => {
   return (
-    <div>
-      <h1 className='bg-purple-400'>hello world</h1>
-      </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
