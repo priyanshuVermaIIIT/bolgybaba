1cf899d0ded76349607801c2431c2e3106c09639
Priyanshu <20bec076@iiitdmj.ac.in>
2024-06-11 15:32:08 +0530

    made a header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,19 @@ import Dashboard from './pages/Dashboard';
 import SignIn from './pages/SignIn';
 import Signup from './pages/Signup';
 import Projects from './pages/Projects';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<Signup />} />
-        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects" element={<Projects  />} />
       </Routes>
     </BrowserRouter>
   );
