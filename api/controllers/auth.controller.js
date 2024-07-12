cfa1ce69c08b410e121a0887ff602ef7ff9a31a2
Priyanshu <20bec076@iiitdmj.ac.in>
2024-07-12 12:56:37 +0530

    user sign up completed

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,20 +1,35 @@
 import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs'
 
 export const signup =async (req,res)=>{
    const {username,email,password} = req.body;
 
 
-   if(!username || !email || !password || username =='' || email==''||password==''){
-    return res.status(400).json({message : 'All fileds are required'})
-   }
-
+    if (
+      !username ||
+      !email ||
+      !password ||
+      username === "" ||
+      email === "" ||
+      password === ""
+    ) {
+      return res.status(400).json({ message: "All fileds are required" });
+    }
+ const hashedPassword = bcryptjs.hashSync(password , 10);
    const newUser = new User({
     username,
     email,
-    password,
+    password: hashedPassword,
    });
 
-   await newUser.save(); 
-   res.json({message:'sign up sucessfull my friend'})
+   try {
+       await newUser.save(); 
+       res.json('sign up sucessfull my friend')
+    
+    
+   } catch (error) {
+    res.status(500).json({message:error.message});
+   }
+
 
 }
