fcbc80e0b238250197e3e7ff901bcd22039b027a
Priyanshu <20bec076@iiitdmj.ac.in>
2024-07-12 10:58:42 +0530

    create the test api route

--- api/index.js
+++ api/index.js
@@ -1,19 +1,26 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js '
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO)
-.then(
-    ()=>{console.log('database is connected')})
-.catch((err)=>{
+mongoose
+  .connect(
+    "mongodb+srv://vermapriyanshu0004:Priyanshu014@blogy-baba.4ytudao.mongodb.net/?retryWrites=true&w=majority&appName=Blogy-Baba"
+  )
+  .then(() => {
+    console.log("database is connected");
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 const app =express();
 
 
 app.listen(3000,()=>{
     console.log('server is running on port 3000 . Best of luck!')
-})
+});
+
+app.use('/api/user' , userRoutes);
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,10 @@
+
 import React from 'react'
 
-export default function Home() {
+const Home = () => {
   return (
     <div>Home</div>
   )
 }
+
+export default Home
