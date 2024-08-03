26552e5d40e0d849058d558741b0f566771c4f7c
Priyanshu <20bec076@iiitdmj.ac.in>
2024-08-03 15:36:48 +0530

    created sign up page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,34 @@
-import { Button, Label, TextInput } from "flowbite-react";
+import { Alert, Button, Label, TextInput } from "flowbite-react";
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Signup() {
+  const [fromData , setFromData] = useState({});
+  const [errorMessage , setErrorMessage] = useState(null);
+  const [loading , setLoading] = useState(false);
+  const handleChange=(e)=>{
+    setFromData({...fromData , [e.target.id] : e.target.value.trim()})
+};
+const handleSubmit = async (e)=>{
+  e.preventDefault();
+  if(!fromData.usename || !fromData.email || !fromData.password){
+    setErrorMessage('please fill out all fields ');
+    return;
+  }
+  try {
+    const res = await fetch('api/auth/signup', {
+      method : 'POST' , 
+      headers: {'Content-Type' : 'application/json'},
+      body : JSON.stringify(fromData) 
+    });
+    const data = await res.json();
+    
+  } catch (error) {
+    
+  }
+}
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -22,18 +48,18 @@ export default function Signup() {
         </div>
         {/* right */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
               <Label value="Your Username" />
-              <TextInput type="text" placeholder="Username" id="username" />
+              <TextInput type="text" placeholder="Username" id="username"  onChange={handleChange} />
             </div>
             <div>
               <Label value="Your Email" />
-              <TextInput type="text" placeholder="xyxcompany@gmail.com" id="email" />
+              <TextInput type="email" placeholder="xyxcompany@gmail.com" id="email" onChange={handleChange} />
             </div>
             <div>
               <Label value="Your Password" />
-              <TextInput type="text" placeholder="Password" id="password" />
+              <TextInput type="password" placeholder="Password" id="password" onChange={handleChange} />
             </div>
             <Button gradientDuoTone='purpleToPink' type="submit">
               SIGNUP
@@ -45,6 +71,14 @@ export default function Signup() {
             Sign in
             </Link>
           </div>
+          {
+            errorMessage && (
+             
+              <Alert className="mt-5 " color='failure' >
+                 {errorMessage}
+              </Alert>
+            )
+          }
         </div>
       </div>
     </div>
